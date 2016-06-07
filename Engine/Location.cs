f90a1be4d47096c7b3e8c0fe8c453ec11b6e979b
zersiaxHP <florianbeijers@gmail.comHP@HP-PC>
2016-06-07 22:35:07 +0200

    Added some complex properties in the form of class-based types

--- Engine/Location.cs
+++ Engine/Location.cs
@@ -11,11 +11,23 @@ namespace Engine
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public Item ItemRequiredToEnter { get; set; }
+        public Quest QuestAvailableHere { get; set; }
+        public Monster MonsterLivingHere { get; set; }
+        public Location LocationToNorth { get; set; }
+        public Location LocationToEast { get; set; }
+        public Location LocationToSouth { get; set; }
+        public Location LocationToWest { get; se; }
 
-        public Location(int id, string name, string description) {
+
+
+        public Location(int id, string name, string description, Item itemRequiredToEnter = null, Quest questAvailableHere = null, Monster monsterAvailableHere = null) {
             ID = id;
             Name = name;
             Description = description;
+            ItemRequiredToEnter = itemRequiredToEnter;
+            QuestAvailableHere = questAvailableHere
+            as = MonsterLivingHere;
 
         
         }
