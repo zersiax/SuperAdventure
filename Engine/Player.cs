17b8de25ab562ef1bf74fce0b16b0ffe7d3f57c1
zersiaxHP <florianbeijers@gmail.comHP@HP-PC>
2016-06-11 23:19:42 +0200

    Added a humongous, super ugly, large *ss function. This code will only live as long as the next commit and is only meant as pseudo-code. gods its ugly

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -8,23 +8,21 @@ namespace Engine
 {
     public class Player : LivingCreature
     {
-        
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
-        public List<InventoryItem> Inventory { get; set;  }
+        public Location CurrentLocation { get; set; }
+        public List<InventoryItem> Inventory { get; set; }
+        public List<PlayerQuest> Quests { get; set; }
 
-        public List<PlayerQuest> Quests { get; set;  }
-
-        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints) {
+        public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base(currentHitPoints, maximumHitPoints)
+        {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
             Inventory = new List<InventoryItem>();
             Quests = new List<PlayerQuest>();
-
-
-        } 
-
+        }
     }
 }
---
+++ Engine/RandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Engine
+{
+    public static class RandomNumberGenerator
+    {
+        private static Random _generator = new Random();
+        public static int NumberBetween(int minimumValue, int maximumValue)
+        {
+            return _generator.Next(minimumValue, maximumValue + 1);
+        }
+    }
+}
